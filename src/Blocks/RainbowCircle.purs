f9e6ca2ff57099d39473fdecc130bf5ec5f780ed
Alex Gryzlov <a.gryzlov@adform.com>
2015-11-02 18:05:30 +0200

    prettify

--- src/Blocks/RainbowCircle.purs
+++ src/Blocks/RainbowCircle.purs
@@ -1,6 +1,6 @@
 module Blocks.RainbowCircle where
 
-  import Prelude(return,unit,Unit(),(>>=), bind, id, (++), ($), show, (+), (-), (*), (/))
+  import Prelude(return,unit,Unit(),(>>=), bind, id, (++), ($), show, (+), (-), (*), (/), flip)
   import Control.Monad.Eff
 
   import Math (pi)
@@ -34,28 +34,29 @@ d3.select("svg").append("g")
 
 d3.select(self.frameElement).style("height", height + "px");
 -}
-  
 
---main :: forall eff. Eff (d3 :: D3 | eff) Unit
+  tau = 2.0 * pi
+  n = 500.0
+
+  canvasWidth = 960.0
+  canvasHeight = 960.0
+  outRadius = canvasWidth / 2.0 - 20.0
+  inRadius = outRadius - 80.0
+
+  segment = arc
+            .. outerRadius outRadius
+            .. innerRadius inRadius
+            .. startAngle' id
+            .. endAngle' (\d -> d + tau / n * 1.1)
+
   main = do
-    let tau = 2.0 * pi
-        n = 500.0
-        canvasWidth = 960.0
-        canvasHeight = 960.0
-        outRadius = canvasWidth / 2.0 - 20.0
-        inRadius = outRadius - 80.0
-
     rootSelect "svg" .. append "g"
       .. attr "transform" ("translate(" ++ show (canvasWidth / 2.0) ++ "," ++ show (canvasHeight / 2.0) ++ ")")
       .. selectAll "path"
         .. bindData (range 0.0 tau (tau/n))
       .. enter .. append "path"
-        .. (\s -> (arc
-                     .. outerRadius outRadius
-                     .. innerRadius inRadius
-                     .. startAngle' id
-                     .. endAngle' (\d -> d + tau / n * 1.1)) .. (\a -> attr "d" a s))
-      .. style' "fill" (\d -> hsl (d * 360.0 / tau) 1.0 0.5 ) 
+        .. (\s -> segment .. (flip (attr "d") s))
+      .. style' "fill" (\d -> hsl (d * 360.0 / tau) 1.0 0.5 )
 {-
     rootSelect self.frameElement
       .. style "height" show canvasHeight ++ "px"
